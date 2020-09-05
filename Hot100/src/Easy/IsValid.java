42228ea14c2e578b5f7090f6de4de6eca2d8bc51
mexx <mexx12138@163.com>
2020-09-05 15:58:33 +0800

    有效的括号

--- Hot100/src/Easy/IsValid.java
+++ Hot100/src/Easy/IsValid.java
@@ -5,6 +5,11 @@ import java.util.LinkedList;
 /**
  * @author Jialu Hu
  * @create 2020-09-05 15:37
+ * 有效的括号
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 空字符串可被认为是有效字符串
  */
 public class IsValid {
     public boolean isValid(String s){
