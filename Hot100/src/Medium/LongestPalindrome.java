792e37bbe8d76394b07741be778de9f7ba15356e
mexx <mexx12138@163.com>
2020-10-18 20:49:50 +0800

    最长回文子串

---
+++ Hot100/src/Medium/LongestPalindrome.java
@@ -0,0 +1,110 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jialu Hu
+ * @create 2020-10-18 20:29
+ * 最长回文子串
+ */
+public class LongestPalindrome {
+    //动态规划
+    public String longestPalindrome1(String s) {
+            int n = s.length();
+            boolean[][] dp = new boolean[n][n];
+            String ans = "";
+        for (int l = 0; l < n; l++) {
+            for (int i = 0; i < n - 1; i++) {
+                int j = i + 1;
+                if(l == 0){
+                    dp[i][j] = true;
+                }
+                else if(l == 1){
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else{
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
+                }
+                if(dp[i][j] && l + 1 > ans.length()){
+                    ans = s.substring(i,i+l+1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    //中心扩散
+    public String longestPalindrome2(String s){
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+
+    //Manacher
+    public String longestPalindrome3(String s){
+        int start = 0, end = -1;
+        StringBuffer t = new StringBuffer("#");
+        for (int i = 0; i < s.length(); ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
+        }
+        t.append('#');
+        s = t.toString();
+
+        List<Integer> arm_len = new ArrayList<Integer>();
+        int right = -1, j = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            int cur_arm_len;
+            if (right >= i) {
+                int i_sym = j * 2 - i;
+                int min_arm_len = Math.min(arm_len.get(i_sym), right - i);
+                cur_arm_len = expand(s, i - min_arm_len, i + min_arm_len);
+            } else {
+                cur_arm_len = expand(s, i, i);
+            }
+            arm_len.add(cur_arm_len);
+            if (i + cur_arm_len > right) {
+                j = i;
+                right = i + cur_arm_len;
+            }
+            if (cur_arm_len * 2 + 1 > end - start) {
+                start = i - cur_arm_len;
+                end = i + cur_arm_len;
+            }
+        }
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = start; i <= end; ++i) {
+            if (s.charAt(i) != '#') {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return (right - left - 2) / 2;
+    }
+}
